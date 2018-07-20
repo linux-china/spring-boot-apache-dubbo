07eaf8df7f58a4048600fdbb9976a830bd7e1b62
linux_china <linux_china@hotmail.com>
2018-07-20 11:56:31 -0700

    Dubbo Direct call factory

---
+++ spring-boot-starter-apache-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/DubboDirectCallFactory.java
@@ -0,0 +1,44 @@
+package org.mvnsearch.spring.boot.dubbo;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Dubbo Direct call factory
+ *
+ * @author linux_china
+ */
+public class DubboDirectCallFactory {
+    private static ApplicationConfig application = new ApplicationConfig("DubboConsumerTest");
+    /**
+     * dubbo service stubs
+     */
+    private static Map<String, Object> stubs = new HashMap<>();
+
+    /**
+     * 返回直连dubbo服务
+     *
+     * @param clazz dubbo service interface
+     * @return remote service object
+     */
+    public static <T> T dubbo(String host, int port, Class<T> clazz) {
+        // 直连dubbo服务的URL
+        String url = "dubbo://" + host + ":" + port + "/" + clazz.getCanonicalName();
+        if (!stubs.containsKey(url)) {
+            ReferenceConfig<T> reference = new ReferenceConfig<T>();
+            reference.setApplication(application);
+            // 如果点对点直连，可以用reference.setUrl()指定目标地址，设置url后将绕过注册中心，
+            // 其中，协议对应provider.setProtocol()的值，端口对应provider.setPort()的值，
+            // 路径对应service.setPath()的值，如果未设置path，缺省path为接口名
+            reference.setUrl(url);
+            reference.setInterface(clazz);
+            T stub = reference.get();
+            stubs.put(url, stub);
+        }
+        return (T) stubs.get(url);
+
+    }
+}
