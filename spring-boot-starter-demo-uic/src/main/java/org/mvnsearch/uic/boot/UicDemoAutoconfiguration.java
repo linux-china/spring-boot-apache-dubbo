3237c15828768ebb797a0339f8a70c4d333bc8bd
linux_china <linux_china@hotmail.com>
2018-07-20 11:23:04 -0700

    uic auto configuration

---
+++ spring-boot-starter-demo-uic/src/main/java/org/mvnsearch/uic/boot/UicDemoAutoconfiguration.java
@@ -0,0 +1,36 @@
+package org.mvnsearch.uic.boot;
+
+import org.apache.dubbo.config.spring.ReferenceBean;
+import org.mvnsearch.spring.boot.dubbo.DubboAutoConfiguration;
+import org.mvnsearch.spring.boot.dubbo.DubboBasedAutoConfiguration;
+import org.mvnsearch.uic.AccountManager;
+import org.mvnsearch.uic.UicTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * uic auto configuration
+ *
+ * @author linux_china
+ */
+@Configuration
+@EnableConfigurationProperties(UicProperties.class)
+@AutoConfigureAfter(DubboAutoConfiguration.class)
+public class UicDemoAutoconfiguration extends DubboBasedAutoConfiguration {
+    @Autowired
+    private UicProperties properties;
+
+    @Bean
+    public ReferenceBean<UicTemplate> uicTemplate() {
+        return getConsumerBean(UicTemplate.class, properties.getVersion(), properties.getTimeout());
+    }
+
+    @Bean
+    public ReferenceBean<AccountManager> accountManager() {
+        return getConsumerBean(AccountManager.class, properties.getVersion(), properties.getTimeout());
+    }
+
+}
