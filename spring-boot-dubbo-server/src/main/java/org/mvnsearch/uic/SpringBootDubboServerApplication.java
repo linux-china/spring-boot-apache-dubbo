56aa7b5c33435c88e40073f52018b5b99fd786f9
linux_china <linux_china@hotmail.com>
2018-07-20 11:12:54 -0700

    spring boot dubbo server

---
+++ spring-boot-dubbo-server/src/main/java/org/mvnsearch/uic/SpringBootDubboServerApplication.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.uic;
+
+import org.mvnsearch.spring.boot.dubbo.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDubboConfiguration
+public class SpringBootDubboServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDubboServerApplication.class, args);
+    }
+}
