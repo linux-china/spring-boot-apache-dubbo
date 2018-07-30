bdfdd3587babe0fb20dc741879b3d9e333661e94
linux_china <linux_china@hotmail.com>
2018-07-30 15:52:15 -0700

    adjust code to add catch

--- spring-boot-starter-apache-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/listener/ProviderInvokeStaticsFilter.java
+++ spring-boot-starter-apache-dubbo/src/main/java/org/mvnsearch/spring/boot/dubbo/listener/ProviderInvokeStaticsFilter.java
@@ -2,6 +2,7 @@ package org.mvnsearch.spring.boot.dubbo.listener;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import org.apache.dubbo.common.Constants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
@@ -18,7 +19,12 @@ public class ProviderInvokeStaticsFilter implements Filter {
     @SuppressWarnings("Duplicates")
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String key = invoker.getInterface().getCanonicalName() + "." + invocation.getMethodName();
-        metrics.counter(key).increment();
-        return invoker.invoke(invocation);
+        Timer.Sample sample = Timer.start(metrics);
+        try {
+            return invoker.invoke(invocation);
+        } finally {
+            sample.stop(metrics.timer(key));
+            metrics.counter(key + ".count").increment();
+        }
     }
 }
