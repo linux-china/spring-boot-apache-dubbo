a449ceb2fe291036b039589e58739f96aa069e10
linux_china <libing.chen@gmail.com>
2019-10-17 15:58:39 -0700

    add reactive action

--- spring-boot-dubbo-client/src/main/java/org/mvnsearch/PortalController.java
+++ spring-boot-dubbo-client/src/main/java/org/mvnsearch/PortalController.java
@@ -2,10 +2,13 @@ package org.mvnsearch;
 
 import org.mvnsearch.uic.AccountManager;
 import org.mvnsearch.uic.UicTemplate;
+import org.mvnsearch.uic.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
 /**
  * portal controller
@@ -23,5 +26,11 @@ public class PortalController {
     public String index(Model model) {
         model.addAttribute("user", uicTemplate.findById(1L));
         return "index";
+    }
+
+    @RequestMapping("/reactive/user")
+    @ResponseBody
+    public Mono<User> reactive() {
+        return Mono.fromFuture(uicTemplate.findByIdFuture(1L));
     }
 }
