5984573c1a34dd41fd493fec076ec57e3239fa35
linux_china <libing.chen@gmail.com>
2020-02-09 15:18:59 -0800

    add static

--- spring-boot-dubbo-client/src/main/java/org/mvnsearch/UserController.java
+++ spring-boot-dubbo-client/src/main/java/org/mvnsearch/UserController.java
@@ -1,5 +1,6 @@
 package org.mvnsearch;
 
+import com.github.javafaker.Faker;
 import org.mvnsearch.uic.User;
 import org.mvnsearch.uic.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    private Faker faker = new Faker();
     @Autowired
     private UserService userService;
 
@@ -21,4 +23,15 @@ public class UserController {
     public User user(@PathVariable Integer id) {
         return userService.findById(id);
     }
+
+    @GetMapping("/static")
+    public User staticShow() {
+        User user = new User();
+        user.setId(100);
+        user.setNick(faker.name().name());
+        user.setPhone(faker.phoneNumber().cellPhone());
+        user.setEmail(faker.internet().emailAddress());
+        return user;
+    }
+
 }
