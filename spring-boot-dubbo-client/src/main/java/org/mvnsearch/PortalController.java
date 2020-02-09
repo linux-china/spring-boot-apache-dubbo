adf7a48531aeebbc3b7f8b2555e5b24633aede1a
linux_china <libing.chen@gmail.com>
2020-02-09 13:01:54 -0800

    adjust

--- spring-boot-dubbo-client/src/main/java/org/mvnsearch/PortalController.java
+++ spring-boot-dubbo-client/src/main/java/org/mvnsearch/PortalController.java
@@ -1,14 +1,10 @@
 package org.mvnsearch;
 
-import org.mvnsearch.uic.AccountManager;
 import org.mvnsearch.uic.UicTemplate;
-import org.mvnsearch.uic.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.publisher.Mono;
 
 /**
  * portal controller
@@ -19,18 +15,11 @@ import reactor.core.publisher.Mono;
 public class PortalController {
     @Autowired
     private UicTemplate uicTemplate;
-    @Autowired
-    private AccountManager accountManager;
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("user", uicTemplate.findById(1L));
+        model.addAttribute("user", uicTemplate.findById(1));
         return "index";
     }
 
-    @RequestMapping("/reactive/user")
-    @ResponseBody
-    public Mono<User> reactive() {
-        return Mono.fromFuture(uicTemplate.findByIdFuture(1L));
-    }
 }
